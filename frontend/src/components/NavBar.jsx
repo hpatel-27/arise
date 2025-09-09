ea40d44065e2a1fe8db303b5b6eff49569db2c53
Harsh Patel <harshpxv@gmail.com>
2025-09-09 00:21:05 -0400

    feat: initial navbar with app name, icon, and links to pages

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between p-4 bg-gray-700 mb-4">
+      <div className="flex items-center space-x-2 pl-2">
+        <p className="text-xl font-bold">Arise</p>
+        <img
+          src="/src/assets/sword-jin-woo.png"
+          alt="Pixel art of Sung Jin-Woo's sword"
+          className="w-10 h-10"
+        />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Link
+          to="/"
+          className="bg-blue-400 text-white px-3 py-2 rounded-lg hover:bg-blue-500 cursor-pointer"
+        >
+          Home
+        </Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
