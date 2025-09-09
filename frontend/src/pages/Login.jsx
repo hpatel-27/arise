1b03fee895bf065025f3d57b2571554ee81d2823
Harsh Patel <harshpxv@gmail.com>
2025-09-09 00:20:31 -0400

    feat: login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="bg-amber-600">Card</div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
