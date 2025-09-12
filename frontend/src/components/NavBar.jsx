c5beadb71e732ff0d8c6148b3ad2f380851750ee
Harsh Patel <harshpxv@gmail.com>
2025-09-11 22:10:29 -0400

    feat: using a blurring effect on navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between p-4 bg-gray-700 mb-4">
+    <div className="flex justify-between p-4 mb-4 bg-gray-800/40 backdrop-blur-lg border border-gray-700/40 shadow-lg">
       <div className="flex items-center space-x-2 pl-2">
-        <p className="text-xl font-bold">Arise</p>
+        <p className="text-xl font-bold text-gray-100">Arise</p>
         <img
           src="/src/assets/sword-jin-woo.png"
           alt="Pixel art of Sung Jin-Woo's sword"
@@ -12,36 +12,16 @@ const NavBar = () => {
         />
       </div>
       <div className="flex items-center space-x-4">
-        <Link
-          to="/"
-          className="bg-blue-400 text-white px-3 py-2 rounded-lg hover:bg-blue-500 transition cursor-pointer"
-        >
-          Home
-        </Link>
-        <Link
-          to="/about"
-          className="bg-blue-400 text-white px-3 py-2 rounded-lg hover:bg-blue-500 transition cursor-pointer"
-        >
-          About
-        </Link>
-        <Link
-          to="/users"
-          className="bg-blue-400 text-white px-3 py-2 rounded-lg hover:bg-blue-500 transition cursor-pointer"
-        >
-          Users
-        </Link>
-        <Link
-          to="/login"
-          className="bg-blue-400 text-white px-3 py-2 rounded-lg hover:bg-blue-500 transition cursor-pointer"
-        >
-          Login
-        </Link>
-        <Link
-          to="/register"
-          className="bg-blue-400 text-white px-3 py-2 rounded-lg hover:bg-blue-500 transition cursor-pointer"
-        >
-          Register
-        </Link>
+        {["Home", "About", "Users", "Login", "Register"].map((label) => (
+          <Link
+            key={label}
+            to={`/${label.toLowerCase() === "home" ? "" : label.toLowerCase()}`}
+            className="px-3 py-2 rounded-lg bg-gray-600/50 text-gray-100 hover:bg-gray-700/70 transition-colors"
+          >
+            {" "}
+            {label}
+          </Link>
+        ))}
       </div>
     </div>
   );
