a8bbb8aade3bc5e47e881c68214ad4cb83b3df2e
Harsh Patel <harshpxv@gmail.com>
2025-09-20 20:04:04 -0400

    feat: update App to abstract the Route elements into files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 function App() {
   return (
     <div>
@@ -10,25 +14,13 @@ function App() {
             renders the first one that matches the current URL. */}
       <Routes>
         <Route path="/about" element={<About />}></Route>
-        <Route path="/users" element={<Users />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
       </Routes>
     </div>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
