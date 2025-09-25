5b9d2ba14a8f2b219b9ebb327fde75a6d2fc7469
Harsh Patel <harshpxv@gmail.com>
2025-09-24 23:11:34 -0400

    feat: reactivate the passport setup

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-const app = express();
-const authRoutes = require("./routes/auth");
 
-// const passport = require("passport");
-// require("./config/passport"); // Ensure you have a passport configuration file
+const app = express();
+
+const passport = require("passport");
+require("./config/passport"); // Ensure you have a passport configuration file
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
-// app.use(passport.initialize());
+app.use(passport.initialize());
 
 // routes
+const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
