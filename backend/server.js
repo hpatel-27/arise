1c00ed8601899f207db2526f00d98a7933473278
Harsh Patel <harshpxv@gmail.com>
2025-09-24 22:40:27 -0400

    feat: temporarily disable passport

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const passport = require("passport");
 require("dotenv").config();
-require("./config/passport"); // Ensure you have a passport configuration file
-
+const cors = require("cors");
+const express = require("express");
+const app = express();
 const authRoutes = require("./routes/auth");
 
-const app = express();
-const port = process.env.PORT || 8080;
+// const passport = require("passport");
+// require("./config/passport"); // Ensure you have a passport configuration file
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
 // routes
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Backend server running...");
-});
-
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
