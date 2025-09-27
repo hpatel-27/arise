50d95aa32cd21e217b8dd7a4df06f056f8890633
Harsh Patel <harshpxv@gmail.com>
2025-09-26 22:31:35 -0400

    feat: set the route for getting a user by id

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 // Get all users (for testing purposes)
 router.get("/users", userController.getAllUsers);
+router.get("/users/:id", userController.getUserById);
 
 module.exports = router;
