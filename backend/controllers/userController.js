7f334a4f74c103b741ec23fd1f38fa9efc64666b
Harsh Patel <harshpxv@gmail.com>
2025-09-26 22:18:04 -0400

    feat: call service method to get all users in the user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+const userService = require("../services/userService");
+
+async function getAllUsers(req, res) {
+  try {
+    const users = await userService.getAllUsers();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = { getAllUsers };
