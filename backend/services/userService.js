aea7ace3248c434e9643f18d73d44a492b938713
Harsh Patel <harshpxv@gmail.com>
2025-09-26 22:17:33 -0400

    feat: create user service to get all users and get a user by ID

---
+++ backend/services/userService.js
@@ -0,0 +1,20 @@
+const prisma = require("../db");
+
+async function getAllUsers() {
+  const users = await prisma.user.findMany({
+    select: { id: true, email: true },
+  });
+  //   if (!users) throw new Error("No users found");
+  return users;
+}
+
+async function getUserById(id) {
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: { id: true, email: true },
+  });
+  if (!user) throw new Error("User not found");
+  return user;
+}
+
+module.exports = { getAllUsers };
