7dda0cf1e08a95cd5321372abee12f6811c28756
Harsh Patel <harshpxv@gmail.com>
2025-09-30 00:50:18 -0400

    chore: formatting

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+
 // Get all users (for testing purposes)
 router.get("/users", userController.getAllUsers);
 router.get("/users/:id", userController.getUserById);
