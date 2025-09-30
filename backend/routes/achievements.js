52266985774969170147227ee93d1ed6c77c214e
Harsh Patel <harshpxv@gmail.com>
2025-09-30 12:42:27 -0400

    feat: set routes for getting an achievement and patching them

--- backend/routes/achievements.js
+++ backend/routes/achievements.js
@@ -2,7 +2,13 @@ import express from "express";
 const router = express.Router();
 const achievementController = require("../controllers/achievementController");
 
+// Get all achievements
 router.get("/achievements", achievementController.getAllAchievements);
+// Get a single achievement by ID
+router.get("/achievements/:id", achievementController.getAchievementById);
+// Update an achievement by ID
+router.patch("/achievements/:id", achievementController.updateAchievement);
+// Create a new achievement
 router.post("/achievements", achievementController.createAchievement);
 
 module.exports = router;
