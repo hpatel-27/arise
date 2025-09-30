cff19571e2c837d664a051a7ea2b54887d7f9441
Harsh Patel <harshpxv@gmail.com>
2025-09-30 11:52:41 -0400

    feat: create achievement service and controller method

--- backend/controllers/achievementController.js
+++ backend/controllers/achievementController.js
@@ -9,4 +9,21 @@ async function getAllAchievements(req, res) {
   }
 }
 
-module.exports = { getAllAchievements };
+async function createAchievement(req, res) {
+  console.log("Creating achievement with data:", req.body);
+  try {
+    const data = {
+      name: req.body.name,
+      description: req.body.description,
+      iconPath: req.body.iconPath,
+      category: req.body.category,
+    };
+
+    const achievement = await achievementService.createAchievement(data);
+    res.status(201).json(achievement);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+module.exports = { getAllAchievements, createAchievement };
--- backend/services/achievementService.js
+++ backend/services/achievementService.js
@@ -1,4 +1,5 @@
 import prisma from "../db";
+
 async function getAllAchievements() {
   const achievements = await prisma.achievement.findMany({
     select: { id: true },
@@ -6,4 +7,9 @@ async function getAllAchievements() {
   return achievements;
 }
 
-module.exports = { getAllAchievements };
+async function createAchievement(data) {
+  const achievement = await prisma.achievement.create({ data });
+  return achievement;
+}
+
+module.exports = { getAllAchievements, createAchievement };
