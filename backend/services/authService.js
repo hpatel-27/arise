86b2a826df8ee9e7b0b84245e384be8ea0fbbd34
Harsh Patel <harshpxv@gmail.com>
2025-10-02 10:42:14 -0400

    feat: send a response instead of throwing an error

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,10 +1,12 @@
 const prisma = require("../db");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../utils/jwt");
+const { response } = require("express");
 
 async function register(email, password) {
   const existingUser = await prisma.user.findUnique({ where: { email } });
-  if (existingUser) throw new Error("Email already in use");
+  if (existingUser)
+    return response.status(409).json({ message: "Email already in use" });
 
   const hashedPassword = await bcrypt.hash(password, 10);
   return prisma.user.create({
@@ -14,10 +16,10 @@ async function register(email, password) {
 
 async function login(email, password) {
   const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) throw new Error("Invalid email or password");
+  if (!user) return response.json({ message: "Invalid email or password" });
 
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) throw new Error("Invalid email or password");
+  const isMatch = bcrypt.compare(password, user.password);
+  if (!isMatch) return response.json({ message: "Invalid email or password" });
 
   return generateToken(user.id);
 }
