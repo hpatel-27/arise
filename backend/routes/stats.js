f567706135d8b576d08c1e42eb68e392793af224
Harsh Patel <harshpxv@gmail.com>
2025-11-11 17:42:39 -0500

    feat: secure stat routes and decode user from token

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const statController = require("../controllers/statController");
+const { authenticate } = require("../middleware/authMiddleware");
 
 // Initialize Stats for user
 router.post("/stats/init", statController.initializeStats);
 
 // Get stats of a user
-router.get("/stats/:userId", statController.getStats);
+router.get("/stats/me", authenticate, statController.getStats);
 
 // Get a stat for a user
-router.get("/stats/:userId/:categoryId", statController.getStatByCategory);
+router.get(
+  "/stats/me/:categoryId",
+  authenticate,
+  statController.getStatByCategory
+);
 
 // Update a stat using earned xp
-router.patch("/stats/:userId", statController.updateStat);
+router.patch("/stats/me", authenticate, statController.updateStat);
 
 module.exports = router;
