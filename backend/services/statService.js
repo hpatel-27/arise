b253adb4ca6790476f616224281fe69d2732b41f
Harsh Patel <harshpxv@gmail.com>
2025-11-10 22:14:25 -0500

    feat: GET and update a specific stat

--- backend/services/statService.js
+++ backend/services/statService.js
@@ -1,5 +1,8 @@
 const prisma = require("../db");
 
+// Constants
+const XP_PER_LEVEL = 1000;
+
 // helper function to translate a categoryId to its name
 const translateCategory = (categoryId) => {
   const categories = {
@@ -33,7 +36,7 @@ async function getStats(userId) {
   // Theres only going to be 5 items, due to there being 5 categories
   const stats = [];
   const existingStats = await prisma.userStat.findMany({
-    where: { userId: userId },
+    where: { userId },
   });
 
   existingStats.map((stat) =>
@@ -46,7 +49,41 @@ async function getStats(userId) {
   return stats;
 }
 
+async function getStatByCategory(userId, categoryId) {
+  const stat = await prisma.userStat({ where: { userId, categoryId } });
+  return stat;
+}
+
+async function updateStats(userId, categoryId, xpEarned) {
+  const stat = await prisma.userStat.findFirst({
+    where: { userId, categoryId },
+  });
+
+  let currentXP = stat.currentXP;
+  let currentLevel = stat.statLevel;
+
+  // add xp
+  currentXP += xpEarned;
+
+  // calculate level ups
+  let levelsGained = Math.floor(currentXp / XP_PER_LEVEL);
+  currentLevel += levelsGained;
+
+  // Remainder xp after leveling up
+  currentXP = currentXP % XP_PER_LEVEL;
+
+  return await prisma.userStat.update({
+    where: { id: stat.id },
+    data: {
+      currentXP: currentXP,
+      statLevel: currentLevel,
+    },
+  });
+}
+
 module.exports = {
   initializeStats,
   getStats,
+  getStatByCategory,
+  updateStats,
 };
