c3ca37bf5b59be0a760aad61927b8d5646e9cf1b
Harsh Patel <harshpxv@gmail.com>
2025-12-21 22:21:40 -0500

    feat: get assigned tasks and update error messages

--- backend/controllers/userTaskController.js
+++ backend/controllers/userTaskController.js
@@ -1,5 +1,36 @@
 const userTaskService = require("../services/userTaskService");
 const { userTaskDTO } = require("../dtos/userTask.dto");
+const { userTaskCompletionDTO } = require("../dtos/userTaskCompleted.dto");
+
+async function getAllActiveTasksForUser(req, res) {
+  try {
+    const userId = req.user.userId;
+    const tasks = await userTaskService.getAllActiveTasksForUser(userId);
+    res.json(tasks.map(userTaskDTO));
+  } catch (error) {
+    const status = error.statusCode || 500;
+    res.status(status).json({ error: error.message });
+  }
+}
+
+async function getTaskForUser(req, res) {
+  try {
+    const userId = req.user.userId;
+    let { taskId } = req.params;
+    taskId = parseInt(taskId, 10);
+    if (isNaN(taskId)) {
+      throw new Error("Invalid taskId");
+    }
+    if (taskId <= 0) {
+      throw new Error("TaskId must be greater than 0");
+    }
+    const task = await userTaskService.getTaskForUser(userId, taskId);
+    res.json(userTaskDTO(task));
+  } catch (error) {
+    const status = error.statusCode || 500;
+    res.status(status).json({ error: error.message });
+  }
+}
 
 async function assignTask(req, res) {
   try {
@@ -14,7 +45,7 @@ async function assignTask(req, res) {
     res.json(userTaskDTO(assignedTask));
   } catch (error) {
     const status = error.statusCode || 500;
-    res.status(500).json({ error: error.message });
+    res.status(status).json({ error: error.message });
   }
 }
 
@@ -24,17 +55,22 @@ async function completeTask(req, res) {
     let { taskId } = req.params;
     taskId = parseInt(taskId, 10);
     if (isNaN(taskId)) {
-      return res.status(400).json({ error: "Invalid taskId" });
+      throw new Error("Invalid taskId");
+    }
+    if (taskId <= 0) {
+      throw new Error("TaskId must be greater than 0");
     }
     const completedTask = await userTaskService.completeTask(userId, taskId);
-    res.json(userTaskDTO(completedTask));
+    res.json(userTaskCompletionDTO(completedTask));
   } catch (error) {
     const status = error.statusCode || 500;
-    res.status(500).json({ error: error.message });
+    res.status(status).json({ error: error.message });
   }
 }
 
 module.exports = {
+  getAllActiveTasksForUser,
+  getTaskForUser,
   assignTask,
   completeTask,
 };
