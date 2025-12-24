d9c4891b551a6077ba8bd8c1a547b5266277ae45
Harsh Patel <harshpxv@gmail.com>
2025-12-23 22:45:58 -0500

    feat: add Tasks, Stats, Achievements as protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,41 +4,64 @@ import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import Stats from "./pages/Stats";
+import Achievements from "./pages/Achievements";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <>
-      <AuthProvider>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Landing />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoutes>
-                <Profile />
-              </ProtectedRoutes>
-            }
-          ></Route>
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoutes>
-                <Home />
-              </ProtectedRoutes>
-            }
-          ></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Routes>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/tasks"
+          element={
+            <ProtectedRoutes>
+              <Tasks />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/stats"
+          element={
+            <ProtectedRoutes>
+              <Stats />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/achievements"
+          element={
+            <ProtectedRoutes>
+              <Achievements />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoutes>
+              <Profile />
+            </ProtectedRoutes>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
 
