b715eda12e36a243f7c34a61061f686425eeb023
Harsh Patel <harshpxv@gmail.com>
2025-12-29 16:59:52 -0500

    test: getAllUsers and deleteUsers testing on found and not found cases

--- backend/tests/unit/services/userService.test.js
+++ backend/tests/unit/services/userService.test.js
@@ -1,3 +1,8 @@
+const {
+  mockUserExists,
+  mockUserNotFound,
+} = require("../../helpers/mockUserPrisma");
+
 // mock real prisma client before importing it
 jest.mock("../../../db");
 
@@ -10,6 +15,66 @@ describe("Unit Testing UserService", () => {
   // reset prisma mock state before each test
   beforeEach(() => {
     jest.clearAllMocks();
+  });
+
+  describe("getAllUsers", () => {
+    test("returns the list of users when they exist", async () => {
+      // setup fake data
+      const userId1 = "user-1";
+      const fakeUser1 = {
+        id: userId1,
+        email: "test1@example.com",
+        firstName: "Test1",
+        lastName: "User1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const userId2 = "user-2";
+      const fakeUser2 = {
+        id: userId2,
+        email: "test2@example.com",
+        firstName: "Test2",
+        lastName: "User2",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      // Expect a list of two users
+      prisma.user.findMany.mockResolvedValue([fakeUser1, fakeUser2]);
+      const result = await userService.getAllUsers();
+
+      expect(result).toEqual([fakeUser1, fakeUser2]);
+      expect(prisma.user.findMany).toHaveBeenCalledWith({
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+    });
+
+    test("returns an empty list when there are no users", async () => {
+      // fake data of no users
+      prisma.user.findMany.mockResolvedValue([]);
+      const result = await userService.getAllUsers();
+
+      // Expect empty list
+      expect(result).toEqual([]);
+      expect(prisma.user.findMany).toHaveBeenCalledWith({
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+    });
   });
 
   describe("getUserById", () => {
@@ -51,7 +116,7 @@ describe("Unit Testing UserService", () => {
     test("throw error when user not found", async () => {
       // setup missing user
       const userId = "missing-id";
-      prisma.user.findUnique.mockResolvedValue(null);
+      mockUserNotFound();
 
       // make the actual request to the service
       await expect(userService.getUserById(userId)).rejects.toThrow(
@@ -71,4 +136,40 @@ describe("Unit Testing UserService", () => {
       });
     });
   });
+
+  describe("deleteUser", () => {
+    test("delete an user that exists", async () => {
+      const user = mockUserExists();
+
+      const result = await userService.deleteUser(user.id);
+      expect(result).toEqual(user);
+
+      expect(prisma.user.delete).toHaveBeenCalledWith({
+        where: { id: user.id },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+    });
+
+    test("attempt delete on user that does not exist", async () => {
+      const userId = "missing-id";
+      mockUserNotFound();
+
+      await expect(userService.deleteUser(userId)).rejects.toThrow(
+        `User with id "missing-id" not found.`
+      );
+
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: { id: userId },
+      });
+    });
+  });
+
+  describe("updateUser", () => {});
 });
